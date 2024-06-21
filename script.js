2601838cb56563df9c89c65cb62b8fbd1551ef66
kzk4043 <okazakishoma@team-lab.com>
2024-06-21 09:17:20 +0900

    feat: tdd step2

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const calculator = (args) => {
   const operation = args[0];
   const numbers = args.slice(1).map((num) => parseInt(num, 10));
 
+  // 引数が数字以外だとエラーが発生する
+  if (numbers.some((num) => isNaN(num))) {
+    throw new Error("Invalid number");
+  }
+
+  // 31個以上の引数を指定するとエラーが発生する
+  if (numbers.length > 30) {
+    throw new Error("Too many numbers");
+  }
+
   // operationによって処理を分岐
   switch (operation) {
     case "add":
@@ -27,22 +37,50 @@ const calculator = (args) => {
 
 // 加算
 const add = (numbers) => {
-  return numbers.reduce((acc, num) => acc + num, 0);
+  const result = numbers.reduce((acc, num) => acc + num, 0);
+
+  // 計算結果が1000を超える場合は合計ではなく「too big」と文字列が返る
+  if (result > 1000) {
+    return "too big";
+  }
+
+  return result;
 };
 
 // 減算
 const subtract = (numbers) => {
-  return numbers.reduce((acc, num) => acc - num);
+  const result = numbers.reduce((acc, num) => acc - num);
+
+  // 計算結果がマイナスの場合は「negative number」と文字列が返る
+  if (result < 0) {
+    return "negative number";
+  }
+
+  return result;
 };
 
 // 乗算
 const multiply = (numbers) => {
-  return numbers.reduce((acc, num) => acc * num, 1);
+  const result = numbers.reduce((acc, num) => acc * num, 1);
+
+  // 計算結果が1000を越える場合は「big big number」と文字列が返る
+  if (result > 1000) {
+    return "big big number";
+  }
+
+  return result;
 };
 
 // 除算
 const divide = (numbers) => {
-  return numbers.reduce((acc, num) => acc / num);
+  // 割り算の場合、0で割るとエラーが発生する
+  if (numbers.some((num) => num === 0)) {
+    throw new Error("Cannot divide by zero");
+  }
+
+  const result = numbers.reduce((acc, num) => acc / num);
+  // 計算結果を小数点以下2桁まで表示
+  return Math.round(result * 100) / 100;
 };
 
 calculator(args);
--- script.test.js
+++ script.test.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 
-describe("script.js CLI", () => {
+// 通常の四則演算
+describe("normal calculation", () => {
   // 「node script.js multiply 3 10 3」を実行して、90が返ってくることを確認する
   it("should multiply numbers correctly", () => {
     return new Promise((resolve, reject) => {
@@ -30,9 +31,9 @@ describe("script.js CLI", () => {
   // 「node script.js subtract 3 10 3」を実行して、-10が返ってくることを確認する
   it("should subtract numbers correctly", () => {
     return new Promise((resolve, reject) => {
-      exec("node script.js subtract 3 10 3", (error, stdout, stderr) => {
+      exec("node script.js subtract 20 10 3", (error, stdout, stderr) => {
         try {
-          expect(stdout.trim()).toBe("-10");
+          expect(stdout.trim()).toBe("7");
           resolve();
         } catch (e) {
           reject(e);
@@ -46,6 +47,117 @@ describe("script.js CLI", () => {
       exec("node script.js divide 100 10", (error, stdout, stderr) => {
         try {
           expect(stdout.trim()).toBe("10");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // かけ算の場合、計算結果が1000を越える場合は「big big number」と文字列が返る（境界値1000の場合）
+  it("should return 'too big' if the result is over 1000", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js multiply 100 10", (error, stdout, stderr) => {
+        try {
+          expect(stdout.trim()).toBe("1000");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+});
+
+// 特殊な制限
+describe("special limitations", () => {
+  // 1個〜30個までの引数を受け取る（31個以上の引数を指定するとエラーが発生する）
+  it("should not accept more than 30 props", () => {
+    return new Promise((resolve, reject) => {
+      exec(
+        "node script.js add 1 1 1 1 1  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 11",
+        (error, stdout, stderr) => {
+          try {
+            expect(stderr.trim()).toContain("Too many numbers");
+            resolve();
+          } catch (e) {
+            reject(e);
+          }
+        }
+      );
+    });
+  });
+  // 引数が数字以外だとエラーが発生する
+  it("should not accept non-numeric arguments", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js add 1 1 1 1 1 hoge", (error, stdout, stderr) => {
+        try {
+          expect(stderr.trim()).toContain("Invalid number");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // 足し算の場合、計算結果が1000を超える場合は合計ではなく「too big」と文字列が返る
+  it("should return 'too big' if the result is over 1000", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js add 1000 1", (error, stdout, stderr) => {
+        try {
+          expect(stdout.trim()).toBe("too big");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // 引き算の場合、計算結果がマイナスの場合は「negative number」と文字列が返る
+  it("should return 'negative number' if the result is negative", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js subtract 1 10", (error, stdout, stderr) => {
+        try {
+          expect(stdout.trim()).toBe("negative number");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // かけ算の場合、計算結果が1000を越える場合は「big big number」と文字列が返る
+  it("should return 'big big number' if the result is over 1000", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js multiply 100 11", (error, stdout, stderr) => {
+        try {
+          expect(stdout.trim()).toBe("big big number");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // 割り算の場合、計算結果を小数点以下2桁まで表示
+  it("should return the result with 2 decimal places", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js divide 1 3", (error, stdout, stderr) => {
+        try {
+          expect(stdout.trim()).toBe("0.33");
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+  // 割り算の場合、0で割るとエラーが発生する
+  it("should not divide by zero", () => {
+    return new Promise((resolve, reject) => {
+      exec("node script.js divide 1 0", (error, stdout, stderr) => {
+        try {
+          expect(stderr.trim()).toContain("Cannot divide by zero");
           resolve();
         } catch (e) {
           reject(e);
